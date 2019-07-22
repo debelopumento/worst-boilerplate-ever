cafcd73c8de144a8b836044c5b9276df9501add3
Di Ye <imdiye@gmail.com>
2019-07-21 19:22:37 -0700

    clean up

--- src/page-1.js
+++ src/page-1.js
@@ -4,60 +4,47 @@ import { connect } from "react-redux"
 import * as actions from "./actions"
 import logo from "./logo.svg"
 import "./App.css"
-import axios from "axios"
-const { createApolloFetch } = require("apollo-fetch")
 
-const fetch = createApolloFetch({
-  uri: "https://countries.trevorblades.com/",
-})
+class Page extends React.Component {
+  render() {
+    const { accumulator, increment, decrement, countries } = this.props
 
-function App(props) {
-  const { accumulator, increment, decrement } = props
-  fetch({
-    query: `{
-      countries {
-        name
-        currency
-        phone
-        languages {
-          name
-          native
-        }
-      }
-    }`,
-  }).then(res => {
-    const { countries } = res.data
-    console.log(2020, countries)
-  })
+    return (
+      <div className="App">
+        <Helmet>
+          <meta charSet="utf-8" />
+          <meta
+            name="description"
+            content="The worst boilerplate ever! Built with <3 by Di Ye"
+          />
+          <title>Worst Boilerplate Ever!</title>
+          <link rel="canonical" href="https://github.com/debelopumento" />
+        </Helmet>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>Worst React Boilerplate Ever!</h1>
+          <button onClick={increment}>increment</button>
+          <button onClick={decrement}>decrement</button>
+          <h1>Accumulator: {accumulator}</h1>
+        </header>
+      </div>
+    )
+  }
 
-  return (
-    <div className="App">
-      <Helmet>
-        <meta charSet="utf-8" />
-        <meta
-          name="description"
-          content="The worst boilerplate ever! Built with <3 by Di Ye"
-        />
-        <title>Worst Boilerplate Ever!</title>
-        <link rel="canonical" href="https://github.com/debelopumento" />
-      </Helmet>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Worst React Boilerplate Ever!</h1>
-        <button onClick={increment}>increment</button>
-        <button onClick={decrement}>decrement</button>
-        <h1>Accumulator: {accumulator}</h1>
-      </header>
-    </div>
-  )
+  componentDidMount() {
+    const { fetchAndUpdateCountries } = this.props
+    fetchAndUpdateCountries()
+  }
 }
 
 export default connect(
   storeState => ({
     accumulator: storeState.accumulator,
+    countries: storeState.countries,
   }),
   {
     increment: actions.increment,
     decrement: actions.decrement,
+    fetchAndUpdateCountries: actions.fetchAndUpdateCountries,
   },
-)(App)
+)(Page)
